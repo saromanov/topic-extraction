b6329f748e36bd956cf3ec95a20a3331984cf45c
Sergey Romanov <xxsmotur@gmail.com>
2016-02-11 15:36:30 +0500

    Update Frequency

--- frequency.py
+++ frequency.py
@@ -1,1 +1,13 @@
-from nltk import bigrams
+from nltk import bigrams, stopwords
+from nltk.tokenize import word_tokenize
+from collection import Counter
+
+class Frequency:
+	def __init__(self, data):
+		self.stop = stopwords.words("english")
+		self.data = [word_tokenize(word) for word in data]
+
+	def fit(self, data):
+		result = Counter()
+		result.update([item for item in self.data])
+		return result
